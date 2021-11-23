14dc2f5f8919521048297c038b2eb30da0ee9024
m-swelam <mid2o0o11@gmail.com>
2021-11-23 12:37:08 +0200

    get handlebars ready commit

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const path = require("path");
+const { engine } = require("express-handlebars");
 
 const app = express();
 const Port = process.env.port || 4500;
 
+// use public folder
 app.use(express.static(path.join(__dirname, "public")));
 
+// handlebars engine
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+// import index route
 const indexRouter = require("./routes/index");
 app.use("/", indexRouter);
 
+// server listen
 app.listen(Port);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/", (req, res) => {
-  res.send("Home");
+  res.render('home');
 });
 
 module.exports = router;
